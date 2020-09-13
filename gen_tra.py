fa6e96b9689912cbd6c29f3748b1335e777cde03
fzzfoo <1459758385@qq.com>
2020-09-13 21:57:13 +0800

    202009133

--- gen_tra.py
+++ gen_tra.py
@@ -4,19 +4,43 @@ from gen_eight import generate_eightshaped_tra
 from gen_linear import generate_linear_tra
 from visual import imagefigure
 from divide_grid import get_boundary, get_grid_num, save2txt, cut_tra, random_mask
+import os
+import sys
+import numpy as np
 
 
-def generate_random_gps(base_log=None, base_lat=None, radius=None):
-    radius_in_degrees = radius / 111300
-    u = float(random.uniform(0.0, 1.0))
-    v = float(random.uniform(0.0, 1.0))
-    w = radius_in_degrees * math.sqrt(u)
-    t = 2 * math.pi * v
-    x = w * math.cos(t)
-    y = w * math.sin(t)
-    longitude = y + base_log
-    latitude = x + base_lat
-    return longitude, latitude
+EARTH_RADIUS = 6371.0  # 地球半径 6371
+
+class Logger(object):
+    def __init__(self, filename="Default.log"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
+
+
+def generate_random_gps(base_long=None, base_lat=None, radius=None):
+    # radius_in_degrees = radius / 111300
+    # u = float(random.uniform(0.0, 1.0))
+    # v = float(random.uniform(0.0, 1.0))
+    # w = radius_in_degrees * math.sqrt(u)
+    # t = 2 * math.pi * v
+    # x = w * math.cos(t)
+    # y = w * math.sin(t)
+    # longitude = y + base_log
+    # latitude = x + base_lat
+
+    r = np.random.randn(2)
+    long_increase = math.degrees(radius / (EARTH_RADIUS * math.cos(math.radians(base_lat))))  # 经度增量
+    lat_incerase = math.degrees(radius / EARTH_RADIUS)  # 纬度增量
+    long_offset = long_increase * r[0]
+    lat_offset = lat_incerase * r[1]
+    return base_long + long_offset, base_lat + lat_offset
 
 
 def generate_history_trajectory(arr, num):
@@ -25,16 +49,16 @@ def generate_history_trajectory(arr, num):
     for i in range(num):
         temp = []
         for j in range(m):
-            temp += [generate_random_gps(arr[j][0], arr[j][1], 5000)]
+            temp += [generate_random_gps(arr[j][0], arr[j][1], 2)]
         res += [temp]
     return res
 
 
-def tra_conact(tra_day=6, tra_len=48, dist=10):
+def tra_conact(tra_day=6, circle_num=2, dist=10, tra_len=48,):
     tra = []
     print("轨迹两点距离:{}".format(dist))
     for i in range(tra_day):
-        tra_o = generate_eightshaped_tra(2, dist)[:tra_len]
+        tra_o = generate_eightshaped_tra(circle_num, dist)[:tra_len]
         tra += tra_o
     # print(len(tra))
     return tra
@@ -55,7 +79,6 @@ def same_tra(tra, num):
     return tras
 
 
-
 def get_vocab(tra_lists, name):
     """
     生成vocab.txt
@@ -68,16 +91,17 @@ def get_vocab(tra_lists, name):
         for i in tra:
             if i not in tra_p:
                 tra_p.append(i)
-    file_handle = open('data/{}vocab.txt'.format(name), mode='w')
+    file_handle = open('{}vocab.txt'.format(name), mode='w')
     file_handle.writelines("<m>" + "\n")
     file_handle.writelines("<BOA>" + "\n")
     file_handle.writelines("<EOA>" + "\n")
     file_handle.writelines("*" + "\n")
     # <m> <BOA> <EOA> *
     for t in tra_p:
-        file_handle.writelines(str(t) + "\n")
+        if t != '<m>':
+            file_handle.writelines(str(t) + "\n")
     file_handle.close()
-    print("占{}网格".format(len(tra_p) + 4))
+    print("占用{}网格".format(len(tra_p) + 4))
     print("保存到 data/{}vocab.txt".format(name))
 
     return
@@ -113,10 +137,26 @@ def get_vocab(tra_lists, name):
 # tra_mask = random_mask(tra_cut, tra_length=48, mask_num=10)  # 后48 mask 10
 # save2txt(tra_mask, name='8.validate')
 
+def mkdirs(name):
+    if os.path.exists('data/{}'.format(name)):
+        print("{}已存在，换&删！".format(name))
+        sys.exit()
+    try:
+        os.mkdir('data/{}'.format(name))
+    except Exception:
+        print("创建失败")
+        sys.exit()
+    return 'data/{}/'.format(name)
 
-def generate(train_num=2000, test_num=600, validate=300, name='8_10_5.'):
+
+def generate(train_num=3000, test_num=600, validate_num=300, name='98'):
     tra_all = []
-    num = [train_num, test_num, validate]
+
+    num = [train_num, test_num, validate_num]
+    name = mkdirs(name)
+    sys.stdout = Logger('{}log.txt'.format(name))  # 日志信息
+    print(name)
+    print("train:{}  test:{}  validate:{}".format(train_num, test_num, validate_num))
     name_list = [name + "train", name + "test", name + "validate"]
     # for i in range(3):
     #     tra_single = tra_conact(tra_day=6, tra_len=48)  # 生成一条 6*48=288
@@ -128,20 +168,23 @@ def generate(train_num=2000, test_num=600, validate=300, name='8_10_5.'):
     #     tra_mask = random_mask(tra_cut, tra_length=48, mask_num=10)  # 后48 mask 10
     #     save2txt(tra_mask, name=name_list[i])
     #     tra_all += tra_mask
-    tra_single = tra_conact(tra_day=6, tra_len=48, dist=10)  # 生成一条 6*48=288
-    tra_s = generate_history_trajectory(tra_single, train_num+test_num+validate)  # 生成多条
+    tra_single = tra_conact(tra_day=6, dist=10, circle_num=4, tra_len=98)  # 生成一条 6*48=288
+    tra_s = generate_history_trajectory(tra_single, train_num+test_num+validate_num)  # 生成多条
     # tra_s = same_tra(tra_single, train_num+test_num+validate)  # 每条相同
-    tra_grid, grid_cor = get_grid_num(tra_s, 5)  # 转成网格 km
+    tra_grid, grid_cor = get_grid_num(tra_s, 5, name=name)  # 转成网格 km
     # imagefigure(tra_single[:48], grid_cor=grid_cor)  # 显示yi条
-    imagefigure(tra=None, tras=visual_data(tra_s[:5], start=0, end=48), grid_cor=grid_cor, name=name)
-    tra_cut = cut_tra(tra_grid, length=288)  # 截断
-    tra_mask = random_mask(tra_cut, tra_length=48, mask_num=10)  # 后48 mask 10
+    imagefigure(tra=None, tras=visual_data(tra_s[:10], start=0, end=98), grid_cor=grid_cor, name=name)
+    tra_cut = cut_tra(tra_grid, length=98*6)  # 截断
+    tra_mask = random_mask(tra_cut, tra_length=98, mask_num=10)  # 后48 mask 10
     save2txt(tra_mask[:num[0]], name=name_list[0])
     save2txt(tra_mask[num[0]:num[0]+num[1]], name=name_list[1])
     save2txt(tra_mask[num[0]+num[1]:], name=name_list[2])
     get_vocab(tra_mask, name=name)
+    print("轨迹长度48->98")
     return
 
+
 generate()
+
 
 
